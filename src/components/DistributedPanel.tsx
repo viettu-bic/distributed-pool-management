76b85a5bc254c3ec77b886f42f46fc2893c56a41
viet tu <quyen.luminous@gmail.com>
2024-08-21 12:47:36 +0700

    (+) basic done ui

--- src/components/DistributedPanel.tsx
+++ src/components/DistributedPanel.tsx
@@ -1,5 +1,5 @@
 import {formatEther, pad, parseEther} from "viem";
-import {useEffect} from "react";
+import {useEffect, useState} from "react";
 import {BicFactoryConfig, BicRedeemFactoryConfig, BicRedeemTokenConfig, client} from "@/contract/contractConfig";
 
 const distributedPlan = [
@@ -7,68 +7,88 @@ const distributedPlan = [
         id: 1,
         pool: 'Founding Community',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_FOUNDING_COMMUNITY_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('1650000000')
+        total: parseEther('1650000000'),
+        isDeployed: false,
     },
     {
         id: 2,
         pool: 'Core Team',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_CORE_TEAM_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('400000000')
+        total: parseEther('400000000'),
+        isDeployed: false,
     },
     {
         id: 3,
         pool: 'Strategic partner',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_STRATEGIC_PARTNER_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('150000000')
+        total: parseEther('150000000'),
+        isDeployed: false,
     },
     {
         id: 4,
         pool: 'Private VC Deals',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_PRIVATE_VC_DEALS_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('500000000')
+        total: parseEther('500000000'),
+        isDeployed: false,
     },
     {
         id: 5,
         pool: 'Airdrop Campaigns',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_AIRDROP_CAMPAIGNS_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('250000000')
+        total: parseEther('250000000'),
+        isDeployed: false,
     },
     {
         id: 6,
         pool: 'Community and Ecosystem Development',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_COMMUNITY_AND_ECOSYSTEM_DEVELOPMENT_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('300000000')
+        total: parseEther('300000000'),
+        isDeployed: false,
     },
     {
         id: 7,
         pool: 'Operations Fund',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_OPERATIONS_FUND_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('500000000')
+        total: parseEther('500000000'),
+        isDeployed: false,
     },
     {
         id: 8,
         pool: 'Liquidity and Exchange Reserves',
         unlockAddress: null,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('250000000')
+        total: parseEther('250000000'),
+        isDeployed: false,
     },
     {
         id: 9,
         pool: 'Foundation Reserve',
         unlockAddress: process.env.NEXT_PUBLIC_POOL_FOUNDATION_RESERVE_UNLOCK_ADDRESS,
+        lockAddress: null,
         lockAmount: 0,
-        total: parseEther('1000000000')
+        total: parseEther('1000000000'),
+        isDeployed: false,
     }
 ]
 
 export default function DistributedPanel() {
+    const [plan, setPlan] = useState(distributedPlan)
+
     useEffect(() => {
         sync().catch(console.error)
     }, [])
@@ -78,50 +98,53 @@ export default function DistributedPanel() {
         console.log('bicRedeemTokenImpl: ', bicRedeemTokenImpl)
         for (let i = 0; i < distributedPlan.length; i++) {
             const pool = distributedPlan[i]
-            console.log('pool: ', pool)
             if(pool.unlockAddress) {
                 const lockAddress = await client.readContract({...BicFactoryConfig, functionName: 'computeProxyAddress', args: [bicRedeemTokenImpl, pad(pool.unlockAddress, { size: 32 }) ]})
                 console.log('lockAddress: ', lockAddress)
                 pool.lockAddress = lockAddress
                 const lockCode = await client.getCode({address: lockAddress})
-                if(lockCode === '0x') {
-                    console.log('vesting not deployed yet.')
-                    pool.notDeployed = true
+                console.log('lockCode: ', lockCode)
+                if(lockCode) {
+                    pool.isDeployed = true
                 }
+
             }
             // const lockAddress = await BicRedeemFactory.bicRedeemFactory.getLockAddress(pool.id)
         }
+        setPlan([...distributedPlan])
     }
 
     return (
-        <table className="table-auto">
-            <thead>
+        <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-                <th>#</th>
-                <th>Pool</th>
-                <th>Address</th>
-                <th>Lock remain</th>
-                <th>Total</th>
+                <th scope="col" className="px-6 py-3">#</th>
+                <th scope="col" className="px-6 py-3">Pool</th>
+                <th scope="col" className="px-6 py-3">Address</th>
+                <th scope="col" className="px-6 py-3">Lock remain</th>
+                <th scope="col" className="px-6 py-3">Total</th>
             </tr>
             </thead>
             <tbody>
-            {distributedPlan.map(e => (
-                <tr>
-                    <td>{e.id}</td>
-                    <td>{e.pool}</td>
-                    <td>
-                        {e.unlockAddress ?<><a target="_blank" rel="noopener noreferrer" href={"https://sepolia.etherscan.io/address/" + e.lockAddress}>Locked</a>
-                        <br/>
-                        <a target="_blank" rel="noopener noreferrer" href={"https://sepolia.etherscan.io/address/" + e.unlockAddress}>Unlocked</a></> : 'N/A'}
+            {plan.map(e => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600">
+                    <td className="px-6 py-4">{e.id}</td>
+                    <td className="px-6 py-4">{e.pool}</td>
+                    <td className="px-6 py-4">
+                        {e.unlockAddress ? <><a className="lnk-primary" target="_blank" rel="noopener noreferrer"
+                                                href={"https://sepolia.etherscan.io/address/" + e.lockAddress}>Locked</a>
+                            <br/>
+                            <a className="lnk-primary" target="_blank" rel="noopener noreferrer"
+                               href={"https://sepolia.etherscan.io/address/" + e.unlockAddress}>Unlocked</a></> : 'N/A'}
                     </td>
-                    <td>{e.unlockAddress ? e.notDeployed ?
-                        e.lockAmount :
-                        <button>Create lock pool</button> :
+                    <td className="px-6 py-4">{e.lockAddress ? e.isDeployed ?
+                            e.lockAmount :
+                            <button className="btn-primary">Create lock pool</button> :
                         'N/A'
                     }</td>
-                    <td>{formatEther(e.total)}</td>
-                    <td>
-                        <button>Detail</button>
+                    <td className="px-6 py-4">{formatEther(e.total)}</td>
+                    <td className="px-6 py-4">
+                        <button className="btn-primary">Detail</button>
                     </td>
                 </tr>
             ))}
--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,5 +1,7 @@
-export default function TopBar() {
+import {useState} from "react";
 
+export default function TopBar() {
+    const [openWalletConnect, setOpenWalletConnect] = useState(false)
 
     return (
         <nav className="bg-white w-full">
@@ -11,9 +13,33 @@ export default function TopBar() {
                 <div className="w-full md:block md:w-auto" id="navbar-default">
                     <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li>
-                            <a href="#"
-                               className="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                               aria-current="page">Wallet connect</a>
+
+
+                            <button id="dropdownDividerButton" data-dropdown-toggle="dropdownDivider"
+                                    className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                                    onClick={() => setOpenWalletConnect(!openWalletConnect)}
+                                    type="button">Wallet connect <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true"
+                                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                                        viewBox="0 0 10 6">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                      stroke-width="2" d="m1 1 4 4 4-4"/>
+                            </svg>
+                            </button>
+
+                            <div id="dropdownDivider"
+                                 className={"z-10 " + (openWalletConnect || 'hidden') + " font-normal bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600"}>
+                                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                    aria-labelledby="dropdownDividerButton">
+                                    <li>
+                                        <a href="#"
+                                           className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Metamask</a>
+                                    </li>
+                                </ul>
+                                <div className="py-2">
+                                    <a href="#"
+                                       className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Wallet connect</a>
+                                </div>
+                            </div>
                         </li>
                     </ul>
                 </div>
